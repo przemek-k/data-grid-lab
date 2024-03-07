adace07275391b03542b0b591f426a9e196e6b15
Przemek <przemyslaw.kalka@gmail.com>
2024-03-07 10:51:52 +0000

    Productionising css

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,7 @@
-const tailwindcss = require("tailwindcss");
-module.exports = { plugins: ["postcss-preset-env", tailwindcss] };
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+  },
+};
